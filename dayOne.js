8371648b54270843ab5c87238a3c0b0789ad34df
christopher-owusu <60077916+christopher-owusu@users.noreply.github.com>
2023-05-01 22:16:59 +0000

    Add files via upload

---
+++ dayOne.js
@@ -0,0 +1,44 @@
+
+// comments can make code readable
+// Welcome to 30DaysOfJavaScript
+
+/* 
+comments can make code readable,
+easy to reuse and informative
+*/
+
+
+// Variable.js file
+let name = "Name"
+let trueOrFalse = true
+let undefinedd 
+let nulll = null
+
+
+// datatypes.js file
+console.log(typeof name)
+console.log(typeof trueOrFalse)
+console.log(typeof undefinedd)
+console.log(typeof nulll)
+
+
+// our variables without assigning values
+let one
+let two
+let third
+let four
+
+
+// our variables with assigned values
+let varOne = true
+let varTwo = "two"
+let varThird = "three"
+let varFour = 4
+
+let firstName = "Chris", lastName = "Owusu", maritalStatus = "not married", country = "Ghana";
+
+let myAge = 23
+let yourAge = 53
+
+console.log(`I am ${myAge} years old.`)
+console.log(`You are ${yourAge} years old.`)
